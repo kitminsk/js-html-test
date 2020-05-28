07dbc5e587c1deda80d079c04f4a63d35ea7d0b2
kitminsk <kitminsk@gmail.com>
2020-05-28 20:46:36 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+print 'aaaaa';
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/script1.js"></script>
+</head>
+
+<body>
+ BODY
+</body>
+</html>
---
+++ js/script1.js
@@ -0,0 +1,9 @@
+(function ($) {
+ console.log('asd 1');
+})(jQuery);
+(function ($) {
+  console.log('asd 2');
+  function say(message='Hi') {
+    console.log(message);
+  }
+})(jQuery);
