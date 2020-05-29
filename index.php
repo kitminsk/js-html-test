2707240b3cebf1d222b848ca8f43f9590ee907c4
kitminsk <kitminsk@gmail.com>
2020-05-29 10:35:02 +0300

    f

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
-<?php
-print 'aaaaa';
-?>
-
 <!doctype html>
-
 <html lang="en">
-<head>
-  <meta charset="utf-8">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>The HTML5 Herald</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
 
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <div class="container-fluid">
+      Sample text from DEN
+    </div>
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/script1.js"></script>
-</head>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-<body>
- BODY
-</body>
+    <script src="js/script1.js"></script>
+    <script src="js/script2.js"></script>
+
+    <?php
+      print PHP_EOL;
+      phpinfo();
+    ?>
+  </body>
 </html>
---
+++ js/script2.js
@@ -0,0 +1,9 @@
+(function ($) {
+ console.log('asd 2222222222222222');
+})(jQuery);
+(function ($) {
+  console.log('asd 33333333333');
+  function say(message='Hi') {
+    console.log(message);
+  }
+})(jQuery);
